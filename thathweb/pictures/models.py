91d0cdd01d452b11913540701ef4e35c5e441317
Travis Hathaway <travis.j.hathaway@gmail.com>
2013-08-22 22:43:14 -0700

    more changes, found a good way to use isotope. Stuck on json encoding

--- thathweb/pictures/models.py
+++ thathweb/pictures/models.py
@@ -1,4 +1,6 @@
+import json
 from django.db import models
+from django.core.serializers.json import DjangoJSONEncoder
 
 class PictureTag(models.Model):
     title   = models.CharField(max_length=255)
@@ -12,7 +14,16 @@ class Picture(models.Model):
     path        = models.FilePathField(max_length=255)
     thumbnail_path = models.FilePathField(max_length=255)
     date_time   = models.DateTimeField(auto_now=True)
-    picture_tag = models.ForeignKey(PictureTag, null=True, blank=True)
+    picture_tag = models.ManyToManyField(PictureTag, null=True, blank=True)
 
     def __unicode__(self):
         return self.title
+
+    def json_safe(self):
+        return {
+            'title' : self.title,
+            'path'  : self.path,
+            'thumbnail_path' : self.thumbnail_path,
+            'date_time' : self.date_time,
+            'picture_tag' : [ {'name' : tag.name, 'title' : tag.title} for tag in self.picture_tag.all() ]
+        }
--- thathweb/pictures/views.py
+++ thathweb/pictures/views.py
@@ -1,9 +1,9 @@
 import json
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 from django.core import serializers
 from django.template.loader import render_to_string
-from django.conf import settings 
+from thathweb import settings 
 from thathweb.views import ThathwebBaseViewNoAuth
 import models
 
@@ -11,26 +11,29 @@ class Pictures(ThathwebBaseViewNoAuth):
     template_name = "pictures/index2.html"
 
     def get(self, request, *args, **kwargs):
-        if not kwargs.get('page'):
-            page = 1
-        else:
-            page = kwargs.get('page')
-
         pictures  = models.Picture.objects.all()
-        paginator = Paginator(pictures, 25)
-        results   = paginator.page(page)
+        paginator = Paginator(pictures, 5)
+        page = kwargs.get('page')
+
+        try:
+            pictures = paginator.page(page)
+        except PageNotAnInteger:
+            pictures = paginator.page(1)
+        except EmptyPage:
+            pictures = paginator.page(paginator.num_pages)
 
         if request.is_ajax():
-            html = render_to_string('pictures/list_partial.html', {'pictures' : results})
-            print settings.STATIC_URL
-            ret_json = json.dumps({'html' : html, 'STATIC_URL' : settings.STATIC_URL})
-            #ret_json = serializers.serialize('json', {'pictures' : results, 'html' : html})
+            html = render_to_string(
+                'pictures/list_partial.html', 
+                {'pictures' : pictures, 'STATIC_URL' : settings.STATIC_URL}
+            )
+            #ret_json = json.dumps({'html' : html, 'page': page, 'pictures': pictures})
+            ret_json = serializers.serialize('json', pictures)
+            ret_json += json.dumps({'html' : html, 'page': page})
             response = HttpResponse(ret_json, mimetype="application/json" )
         else:
             response = self.render_to_response( { 
-                'pictures' : results,
-                'pk' : kwargs.get('pk'),
-                'paginator' : paginator 
+                'pictures' : pictures,
             } )
 
         return response
