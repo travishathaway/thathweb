5a0a287c23d6ef28e6ce2a27920925c2a6854cd1
Travis Hathaway <travis.j.hathaway@gmail.com>
2013-09-03 09:35:36 -0700

    very rough tag filtering

--- thathweb/pictures/views.py
+++ thathweb/pictures/views.py
@@ -1,6 +1,8 @@
 import json
+from pprint import pprint
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.core import serializers
 from django.template.loader import render_to_string
@@ -14,48 +16,79 @@ class Pictures(ThathwebBaseViewNoAuth):
     def get(self, request, *args, **kwargs):
         self.pictures  = models.Picture.objects.all()
         paginator = Paginator(self.pictures, 20)
-        page = kwargs.get('page')
+        self.page = kwargs.get('page')
 
         #set the tags
         self.get_tags()
 
         try:
-            pictures = paginator.page(page)
+            self.pictures = paginator.page(self.page)
         except PageNotAnInteger:
-            pictures = paginator.page(1)
+            self.pictures = paginator.page(1)
         except EmptyPage:
-            pictures = paginator.page(paginator.num_pages)
+            self.pictures = paginator.page(paginator.num_pages)
 
         if request.is_ajax():
-            pictures_html = render_to_string(
-                'pictures/list_partial.html', 
-                {'pictures' : pictures, 'STATIC_URL' : settings.STATIC_URL}
-            )
-
-            paginator_html = render_to_string(
-                'pictures/pagination.html',
-                { 'pictures' : pictures }
-            )
-
-            tags_html = render_to_string(
-                'pictures/tags.html',
-                {'tags' : self.tags }
-            )
-
-            ret_json = json.dumps({
-                'pictures_html' : pictures_html,
-                'paginator_html' : paginator_html,
-                'page': page
-            })
-
-            response = HttpResponse(ret_json, mimetype="application/json" )
+            response = self.get_ajax()
         else:
             response = self.render_to_response( { 
-                'pictures' : pictures,
+                'pictures' : self.pictures,
                 'tags'     : self.tags
             } )
 
         return response
+
+    def get_ajax(self):
+        # Build all our templates
+        pictures_html = render_to_string(
+            'pictures/list_partial.html', 
+            {'pictures' : self.pictures, 'STATIC_URL' : settings.STATIC_URL}
+        )
+
+        paginator_html = render_to_string(
+            'pictures/pagination.html',
+            { 'pictures' : self.pictures }
+        )
+
+        tags_html = render_to_string(
+            'pictures/tags.html',
+            {'tags' : self.tags }
+        )
+
+        ret_json = json.dumps({
+            'pictures_html' : pictures_html,
+            'paginator_html' : paginator_html,
+            'page': self.page
+        })
+
+        response = HttpResponse(ret_json, mimetype="application/json" )
+
+        return response
+
+    def get_regular(self, request):
+        return
+
+    def post(self, request, *args, **kwargs):
+        self.pictures  = models.Picture.objects.all()
+        self.filter_pictures(request)
+        paginator = Paginator(self.pictures, 20)
+        self.page = kwargs.get('page')
+
+        #set the tags
+        self.get_tags()
+
+        try:
+            self.pictures = paginator.page(self.page)
+        except PageNotAnInteger:
+            self.pictures = paginator.page(1)
+        except EmptyPage:
+            self.pictures = paginator.page(paginator.num_pages)
+
+        return self.get_ajax()
+
+    @csrf_exempt
+    def dispatch(self, *args, **kwargs):
+        return super(Pictures, self).dispatch(*args, **kwargs)
 
     def get_tags(self):
         pt = models.PictureTag.objects.all()
@@ -69,3 +102,9 @@ class Pictures(ThathwebBaseViewNoAuth):
             })
 
         self.tags = tags
+
+    def filter_pictures(self, request):
+        if request.raw_post_data != '':
+            req_json = json.loads(request.raw_post_data)
+            if req_json != []:
+                self.pictures = self.pictures.filter(picture_tag__name__in=req_json);
