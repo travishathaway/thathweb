4c29e56dcd79f879ae90accf9ea144637f8acb1d
Travis Hathaway <travis.j.hathaway@gmail.com>
2013-09-11 13:29:50 -0700

    spruced up the admin page a bit

--- thathweb/pictures/admin.py
+++ thathweb/pictures/admin.py
@@ -5,6 +5,7 @@ class PictureTagAdmin(admin.ModelAdmin):
     prepopulated_fields = { 'name' : ('title',)}
 
 class PictureAdmin(admin.ModelAdmin):
+    list_display = ('thumbnail_admin_view', 'title', 'picture_tag_admin_view')
     exclude = ('path', 'thumbnail_path')
 
 admin.site.register(Picture, PictureAdmin)
--- thathweb/pictures/models.py
+++ thathweb/pictures/models.py
@@ -1,5 +1,7 @@
 import json
+from thathweb import settings 
 from django.db import models
+from django.utils.html import format_html
 
 class PictureTag(models.Model):
     title   = models.CharField(max_length=255)
@@ -21,6 +23,20 @@ class Picture(models.Model):
 
     def __unicode__(self):
         return self.title
+
+    def thumbnail_admin_view(self):
+        return format_html('<img src="{0}{1}" title="{2}" height="40">',
+                           settings.STATIC_URL,
+                           self.thumbnail_path,
+                           self.title)
+
+    def picture_tag_admin_view(self):
+        tag_string = ''
+        for tag in self.picture_tag.all():
+            tag_string += '<span style="background-color: #ccc">' +\
+                    tag.title + '</span>&nbsp;'
+
+        return format_html(tag_string)
 
     def json_safe(self):
         return {
