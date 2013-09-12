a4c339c3655c57fae8a1a41f9aa1b57b3e1cf1b5
Travis Hathaway <travis.j.hathaway@gmail.com>
2013-09-12 08:18:09 -0700

    added some more stuff to the admin file

--- thathweb/pictures/admin.py
+++ thathweb/pictures/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from models import *
 
+def publish(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.published = True
+        obj.save()
+
+def unpublish(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.published = False 
+        obj.save()
+
 class PictureTagAdmin(admin.ModelAdmin):
     prepopulated_fields = { 'name' : ('title',)}
 
 class PictureAdmin(admin.ModelAdmin):
-    list_display = ('thumbnail_admin_view', 'title', 'picture_tag_admin_view')
+    list_display = ('thumbnail_admin_view', 'title', 'picture_tag_admin_view', 'published')
+    list_filter = ('picture_tag','date_time',)
     exclude = ('path', 'thumbnail_path')
+    actions = [publish, unpublish]
 
 admin.site.register(Picture, PictureAdmin)
 admin.site.register(PictureTag, PictureTagAdmin)
